41584f79995ed19a1ffbc2819de3d86239c9a4d8
kasi Durgarao <kasidurgarao183@gmail.com>
2024-11-23 12:28:22 +0530

    Did some modification on enable and disable buttons

--- DOM2.js
+++ DOM2.js
@@ -1,11 +1,10 @@
-
 const btnMoveRight = document.getElementById("moveRight");
 const btnMoveLeft = document.getElementById("moveLeft");
 const btnMoveAllRight = document.getElementById("moveAllRight");
 const btnMoveAllLeft = document.getElementById("moveAllLeft");
 const leftCont = document.querySelector(".left");
 const rightCont = document.querySelector(".right");
-console.log(rightCont.children.length)
+console.log(rightCont.children.length);
 
 btnMoveRight.addEventListener("click", () => moveSelected(leftCont, rightCont));
 btnMoveLeft.addEventListener("click", () => moveSelected(rightCont, leftCont));
@@ -13,25 +12,57 @@ btnMoveAllRight.addEventListener("click", () => moveAll(leftCont, rightCont));
 btnMoveAllLeft.addEventListener("click", () => moveAll(rightCont, leftCont));
 
 function moveAll(from, to) {
-  Array.from(from.children).forEach(item => to.appendChild(item));
+  // Array.from(from.children).forEach(item => to.appendChild(item));
+  // console.log(Array.from(from.children))
+  Array.from(from.children).forEach((item) => {
+    const chk = item.querySelector("input[type='checkbox']");
+    if (chk && chk.checked) {
+      chk.checked = false;
+    }
+    to.appendChild(item);
+    let rightdisable = from.getAttribute("class");
+    console.log(rightdisable);
+    if (rightdisable === "left") {
+      btnMoveAllRight.disabled = true;
+      btnMoveRight.disabled = true;
+    } else {
+      btnMoveAllRight.disabled = false;
+    }
+
+    let leftdisable = from.getAttribute("class");
+    if (leftdisable === "right") {
+      btnMoveAllLeft.disabled = true;
+      btnMoveLeft.disabled = true;
+    } else {
+      btnMoveAllLeft.disabled = false;
+    }
+  });
 }
 
 function moveSelected(from, to) {
-  Array.from(from.children).forEach(item => {
+  Array.from(from.children).forEach((item) => {
     const chk = item.querySelector("input[type='checkbox']");
     if (chk && chk.checked) {
       chk.checked = false;
       to.appendChild(item);
+      btnMoveRight.disabled = true;
+      btnMoveLeft.disabled = true;
     }
   });
 }
 
 function updateBtns() {
-  const leftHasSelected = Array.from(leftCont.querySelectorAll("input[type='checkbox']:checked")).length > 0;
-  const rightHasSelected = Array.from(rightCont.querySelectorAll("input[type='checkbox']:checked")).length > 0;
+  const leftHasSelected =
+    Array.from(leftCont.querySelectorAll("input[type='checkbox']:checked"))
+      .length > 0;
+  const rightHasSelected =
+    Array.from(rightCont.querySelectorAll("input[type='checkbox']:checked"))
+      .length > 0;
 
   btnMoveRight.disabled = !leftHasSelected;
   btnMoveLeft.disabled = !rightHasSelected;
 }
 updateBtns();
-[leftCont, rightCont].forEach(cont => cont.addEventListener("change", updateBtns));
+[leftCont, rightCont].forEach((cont) =>
+  cont.addEventListener("change", updateBtns)
+);
